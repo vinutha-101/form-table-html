6b64283be260456265156652c38da045a55385ef
vinutha-101 <nvinutha101@gmail.com>
2025-02-06 10:36:04 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,9 +1,28 @@
 $(".title").css("font-size","xx-large")
 
-$(document).ready(function() {
-    $("#cancelBtn").click(function() {
-        $("form")[0].reset();
-        window.location.href = "index.html"; 
-    });
-});
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $file = 'products.json';
+    $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+    
+    $newEntry = [
+        "name" => $_POST["name"],
+        "age" => $_POST["age"],
+        "select" => $_POST["select"],
+        "city" => $_POST["city"],
+        "hobbies" => $_POST["hobbies"],
+        "gender" => $_POST["gender"]
+    ];
 
+    if (isset($_POST["index"]) && $_POST["index"] !== "") {
+        $index = (int)$_POST["index"];
+        $data[$index] = $newEntry;  // Update existing entry
+    } else {
+        $data[] = $newEntry;  // Add new entry
+    }
+
+    file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
+    header("Location: index.html"); // Redirect back to the table
+    exit();
+}
+?>
